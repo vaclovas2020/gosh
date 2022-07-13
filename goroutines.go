662babf31102a15a61c45a32dcd4d81ee073d8fb
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-07-13 19:17:43 +0300

    goroutines test fixes

--- goroutines.go
+++ goroutines.go
@@ -3,20 +3,24 @@ package gosh
 import "fmt"
 
 func GoRoutinesTest() {
+	fmt.Println()
 	count := 4
 	buff := make(chan string, count)
+	done := make(chan bool, 1)
 	for i := 0; i < count; i++ {
 		go increment(i, buff)
 	}
-	go printWorker(buff, count)
+	go printWorker(buff, count, done)
+	<-done
 }
 
 func increment(in int, buff chan<- string) {
 	buff <- fmt.Sprintf("%v", in)
 }
 
-func printWorker(buff <-chan string, count int) {
+func printWorker(buff <-chan string, count int, done chan<- bool) {
 	for i := 0; i < count; i++ {
 		fmt.Println(<-buff)
 	}
+	done <- true
 }
